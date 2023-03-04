735b21b95fbaaeb2a93848f2c4d14305ca8c9b51
hadif <hadif2@educbe.ca>
2023-03-04 15:14:46 -0700

    Update Main file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        two();
+    }
+
+    // Question Two
+    static void two() {
+        int[][] intArray = new int[4][4];
+
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; i < 4; i++)
+                intArray[i][j] = (i + 1) * (j + 1);
+
+        for (int[] arr : intArray) {
+            for (int num : arr)
+                System.out.printf("%d ", num);
+
+            System.out.println();
+        }
     }
 }
